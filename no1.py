0c7a48f511300d46cd0dc86af3a6e34609907ad8
KamalMTC <mukhtarlikamal1@gmail.com>
2026-03-13 19:52:59 +0400

    Update no1.py

--- no1.py
+++ no1.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.title("сайт привет")
 st.write("ты умрешь в муках")
 st.write("на тебя наложено проклятие")
