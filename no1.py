e52f220e154253ede65de65c229b792a3fd06fab
KamalMTC <mukhtarlikamal1@gmail.com>
2026-03-13 19:56:38 +0400

    Update no1.py

--- no1.py
+++ no1.py
@@ -5,9 +5,15 @@ hide_st_style = """
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
+            
+            div[data-testid="stStatusWidget"] {visibility: hidden;}
+            .stAppDeployButton {display:none;}
+            [data-testid="stToolbar"] {visibility: hidden !important; display: none !important;}
+            footer {display: none !important;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
+
 
 st.title("сайт привет")
 st.write("ты умрешь в муках")
